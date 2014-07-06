f76fc2657c66a089ba3987fe0efba7d7d6b979a8
searchzakir <searchzakir@gmail.com>
2014-07-07 01:48:25 +0530

    FIND & DELETE the Bogus & Corrupted Mails from Postfix Mail Queue for better performance

--- remove-bogus-corrupted-mails.sh
+++ remove-bogus-corrupted-mails.sh
@@ -6,9 +6,21 @@
 logger -s "running script ($0) by user (`whoami`) and pid = ($$)"
 
 #
-# Checking bogus-corrupted-mails in the Postfix Mail Queues
+# CHECKING & DELETING bogus-corrupted-mails in the Postfix Mail Queues
 #
 # This script to be run on the postfix mail server/instace itself
+# This script should be run every HOURLY or whatever time gap you want but it totally Depends on the mail server load  
+# & the mail queue size.
+# Script will take too much time for extremely large mail queue size
+
+# When the server load is HIGH due to mail load [obvioulsy because of too much disk i/o],
+# the "mailq" command does not respond & hangs.
+# Best workaround is to get an idea of HOW MUCH SIZE is of mail queue rather than finding HOW MANY mails are there
+# And it can be easilt achieved using command "du -sch /var/spool/postfix*" [unless you have custom pool location]
+#
+# This script should be invoked by Cron .. say every few hours.
+# Removing bogus & corrupted mails from theh mail queue is a very good idea from the performance perspective
+#
 #
 # Initilazing Output File
 mkdir -p /tmp/zPlayground/
@@ -23,7 +35,8 @@ if [ -s $OUT ]
 	then
 		for i in `awk -F '/' '{print $2}' $OUT`
 		do
-		rm -f /var/spool/postfix/incoming/$i
+			# Delete the bogus / corrupted mails in the postfix mail queue
+			rm -f /var/spool/postfix/incoming/$i
 		done
 	postsuper -p
 	postsuper -s
@@ -36,3 +49,8 @@ fi
 
 # Truncating output file again
 > $OUT
+
+# As this script will be invoked by cron every few hours
+# At the end of the day, total number of bogus & corrupted mails found & deleted are calculated by script " find-bogus-files-count.sh "
+# So ensure that you have both the scripts on the server, as per your need & requirements
+#
