1b6b6f94feafee5368906acb10b5e45d4a1fd047
searchzakir <searchzakir@gmail.com>
2014-07-07 02:02:44 +0530

    To Find, What is My IP Address ?

--- whatismyIP.sh
+++ whatismyIP.sh
@@ -5,6 +5,11 @@
 
 logger -s "running script ($0) by user (`whoami`) and pid = ($$)"
 
+# On a Virtual Machine, be it either on my laptop or the EC2 or the VM's that I've created on the base servers using OpenVZ
+# Many times it is required to know the IPv4 Address attached to it
+# & Thus this small 1-liner script comes to rescue the situation & make the job trivial
+#
+#
 
 #curl -L0 http://checkip.org | grep 'IP Address' | awk -F '>' '{ print $3 }' | awk -F '<' '{ print $1 }'
 
@@ -14,4 +19,4 @@ curl http://wooledge.org/myip.cgi
 
 #time curl ifconfig.me
 #time curl http://wooledge.org/myip.cgi
-
+# As of today, the wooledge.org is much much faster than ifconfig.me
