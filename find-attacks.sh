790931d70447ec92877fadd8dc58950159f3af07
searchzakir <searchzakir@gmail.com>
2014-07-07 01:33:42 +0530

    find irc bot atacks on server

--- find-attacks.sh
+++ find-attacks.sh
@@ -7,7 +7,7 @@ logger -s "running script ($0) by user (`whoami`) and pid = ($$)"
 
 # Be sure to Replace  EMAIL_@_ADDRESS_HERE with your Email Address 
 #
-# Script to find out the IRC Bot Attack on the server
+# Script to find out the IRC Bot Attack on the server  <---
 # Find any established connections to the IRC ports on the remote server
 #
 # Run this script every few minutes, as per the need
@@ -28,7 +28,7 @@ then
 
 	for i in `netstat -pant | egrep -w '(6660|6667|7000)' | awk '{print $7}' | awk -F '/' '{print $1}'`
 	do
-	kill -9 $i
+		kill -9 $i
 	done
 
 	echo "===================================================" >> $OUTFile
@@ -40,3 +40,4 @@ then
 else
 echo > /dev/null
 fi
+
